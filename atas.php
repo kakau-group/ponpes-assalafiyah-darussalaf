21122795b806d1483795b4d01432ce5f5aa24d85
fadjar setiawan <fadjarsetiawans@gmail.com>
2021-04-07 14:19:35 +0700

    perbaiki email atas

--- atas.php
+++ atas.php
@@ -41,7 +41,7 @@
   <section id="topbar" class="d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
       <div class="contact-info d-flex align-items-center">
-        <i class="bi bi-envelope-fill"></i><a href="darussalafyysn@gmail.com">darussalafyysn@gmail.com</a>
+        <i class="bi bi-envelope-fill"></i><a href="#">darussalafyysn@gmail.com</a>
         <i class="bi bi-phone-fill phone-icon"></i> 0231 635487
       </div>
       <div class="social-links d-none d-md-block">
